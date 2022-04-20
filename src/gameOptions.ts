dd39fa3e72e714deaaff71e025259076c278110c
Mathieu Robichaud <admin@birkoss.com>
2022-04-20 06:45:41 -0400

    Cleanup

--- src/gameOptions.ts
+++ src/gameOptions.ts
@@ -4,9 +4,5 @@ export const GameOptions = {
     tileWidth: 11,
     gameGridScale: 11,
  
-    // number of rows, that is the amount of tries to guess the word
     rows : 6,
- 
-    // vertical position of the first row, starting from the top of the screen
-    firstRowY : 150
 }
